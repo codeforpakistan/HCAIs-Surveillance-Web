bd965ed57323ce75b6514118fc58e5453d1b61a2
Mubassir Hayat <mubassirhayat@gmail.com>
2022-02-04 14:34:17 +0500

    feat: hcai module

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './users';
 import { HospitalList } from './hospitals';
+import { HcaiList } from './hcai';
 import authProvider from './authProvider';
 
 const dataProvider = jsonServerProvider('http://18.139.146.61:3000');
@@ -10,6 +11,7 @@ const App = () => (
   <Admin dataProvider={dataProvider}>
     <Resource name="users" list={UserList} />
     <Resource name="hospitals" list={HospitalList} />
+    <Resource name="hcai" list={HcaiList} />
   </Admin>
 );
 
