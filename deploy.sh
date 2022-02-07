3344b1ff871dfc382680bd93acc3ea4ec62bb07e
Mubassir Hayat <mubassirhayat@gmail.com>
2022-02-07 19:20:36 +0500

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+set -e
+
+echo "Deploying the application ..."
+source ~/.nvm/nvm.sh
+nvm use stable
+echo "Installing dependencies"
+yarn install
+echo "Running the application"
+pm2 restart api
+
+echo "Deployment complete."
